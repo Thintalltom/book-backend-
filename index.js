9fe311cbf18778668bd790382107e264f8d568b0
tomide <tomideadeyanju@gmail.com>
2022-12-21 13:35:13 -0800

    changes mamde

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 app.use(express.json());
 const bodyParser = require("body-parser");
 const PORT = 5000
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/books',express.static('./upload/images') )
-app.use(bodyParser.urlencoded({extended: true}))
+
 app.use('/books', require('./api/book'))
 app.listen(process.env.PORT || PORT, () => {
     console.log('nOW ON PORT 5000')
