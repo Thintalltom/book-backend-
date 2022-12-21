6120ef14d7beb8267f76b71b2dea21e4b3599bb7
tomide <tomideadeyanju@gmail.com>
2022-12-21 08:37:16 -0800

    changes made

--- api/book.js
+++ api/book.js
@@ -11,6 +11,7 @@ db.connect((err) => {
     }
     console.log('connected to db')
 })
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './upload/images')
@@ -23,36 +24,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage
 })
-router.get('/', (req, res) => {
-    //step 1 select all elements in the table
-        db.query('SELECT * FROM books', (err, result) => {
-            if(err) {
-                res.status(400).json(err)
-            } else
-            {
-                res.json(result) 
-            }
-           
-        })
-})
 
-router.post('/', (req, res)=> {
-    // to upload data into the server you have to state the data 
-    const name = req.body.name;
-    const image = req.body.image;
-    const values = [name, image]
-   
-    db.query("INSERT INTO books (name, image) VALUES (?, ?) ",values, (err, result) => {
-        if(err)
-        {
-            res.status(400).json(err)
-        }else
-        {
-            res.status(200).json(result);
-        }
-    })
-    
-})
 
 
 
