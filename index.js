b8a8a8ade58233191498275ca5d63b9599646104
tomide <tomideadeyanju@gmail.com>
2022-12-21 05:57:02 -0800

    changes made

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+
+app.use( function (res, req)  {
+    res.send('hello world')
+})
+app.listen(process.env.PORT || 5000, () => {
+    console.log('nOW ON PORT 5000')
+})
