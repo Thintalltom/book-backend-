957b3fb317db0e1ed9af0d03184aa44096e1dbdf
tomide <tomideadeyanju@gmail.com>
2022-12-22 12:27:22 -0800

    changes made

--- api/book.js
+++ api/book.js
@@ -5,6 +5,18 @@ let book = require('../book')
 const db= require('../config/database')
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+
+fs.readFile('path/to/your/file', (error, data) => {
+  if (error) {
+    throw error;
+  }
+
+  // data is a Buffer object containing the contents of the file
+});
+
+
+
 db.connect((err) => {
     if(err) {
         throw err
@@ -13,7 +25,7 @@ db.connect((err) => {
 })
 
 
-const upload = multer({ dest: 'tmp/' })
+
 
 router.get('/', (req, res) => {
     //step 1 select all elements in the table
@@ -32,14 +44,18 @@ router.get('/', (req, res) => {
 router.post('/',upload.single('books'), (req, res)=> {
     const Author = req.body.Author;
     const title = req.body.title;
-    const image = req.file
+    const image =  Buffer.from(data).toString('utf8');
     const description = req.body.description
 
-    db.query("INSERT INTO economic (Author, title, image, description) VALUES (?,  ?, ?, ? ) ",[ Author, title, image, description], (err, result) => {
+    db.query("INSERT INTO economic (Author, title, image, description) VALUES LOAD_FILE('/path/to/file.txt') (?,  ?, ?, ? ) ",[ Author, title, image, description], (err, result) => {
         if(err)
         {
             res.status(400).json(err)
-        }else
+        } else if(image)
+        {
+            console.log('image usccessfully upload')
+        }
+        else
         {
             res.status(200).json(result);
         }
