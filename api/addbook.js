e3748f97171d46d25a0d800b4b16ea41f6902f9a
tomide <tomideadeyanju@gmail.com>
2022-12-28 16:31:57 -0800

    changes made

--- api/addbook.js
+++ api/addbook.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const multer =require('multer')
 const addbook = require('../addbook')
 const mysql = require ('mysql2')
 const path = require('path')
